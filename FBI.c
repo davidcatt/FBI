d94ed164cd19e9ae30608af84c1b9832af6719a3
David Catt <david.werecat@gmail.com>
2014-06-11 14:33:54 -0400

    Automated cleanup

--- FBI.c
+++ FBI.c
@@ -28,16 +28,16 @@ typedef struct IntStack {
 } IntStack, *PIntStack;
 PIntStack IntStack_new() {
 	PIntStack ni = (PIntStack) malloc(sizeof(IntStack));
-	if(ni == NULL) { return NULL; };
+	if(ni == NULL) { return NULL; }
 	ni->stSz = COLBASE;
 	ni->stPtr = 0;
 	ni->stack = (int*) malloc(sizeof(int) * ni->stSz);
 	if(ni->stack == NULL) {
 		free(ni);
 		return NULL;
-	};
+	}
 	return ni;
-};
+}
 void IntStack_push(PIntStack s, int val) {
 	int* tptr;
 	s->stack[s->stPtr] = val;
@@ -45,22 +45,22 @@ void IntStack_push(PIntStack s, int val) {
 	if(s->stPtr >= s->stSz) {
 		s->stSz <<= 1;
 		tptr = (int*) realloc(s->stack, sizeof(int) * s->stSz);
-		if(tptr == NULL) { free(s->stack); };
+		if(tptr == NULL) free(s->stack);
 		s->stack = tptr;
-	};
-};
+	}
+}
 int IntStack_pop(PIntStack s) {
-	if(s->stPtr <= 0) { return 0; };
+	if(s->stPtr <= 0) return 0;
 	s->stPtr--;
 	return s->stack[s->stPtr];
-};
+}
 void IntStack_free(PIntStack s) {
 	if(s != NULL) {
-		if(s->stack != NULL) { free(s->stack); };
+		if(s->stack != NULL) free(s->stack);
 		free(s);
 		s = NULL;
-	};
-};
+	}
+}
 typedef enum OpCode {
 	NOP = -1,
 	ADJ = 0,
@@ -80,7 +80,7 @@ BFOp BFOp_create(OpCode i, int v) {
 	nb.ins = i;
 	nb.val = v;
 	return nb;
-};
+}
 typedef struct CodeCol {
 	BFOp* ops;
 	int size;
@@ -88,25 +88,25 @@ typedef struct CodeCol {
 } CodeCol, *PCodeCol;
 PCodeCol CodeCol_new() {
 	PCodeCol nc = (PCodeCol) malloc(sizeof(CodeCol));
-	if(nc == NULL) { return NULL; };
+	if(nc == NULL) return NULL;
 	nc->memSz = COLBASE;
 	nc->ops = (BFOp*) malloc(sizeof(BFOp) * nc->memSz);
 	nc->size = 0;
 	if(nc->ops == NULL) {
 		free(nc);
 		return NULL;
-	};
+	}
 	return nc;
-};
+}
 void CodeCol_trim(PCodeCol c) {
 	BFOp* tptr;
 	if(c->size > 0) {
 		c->memSz = c->size;
 		tptr = (BFOp*) realloc(c->ops, sizeof(BFOp) * c->memSz);
-		if(tptr == NULL) { free(c->ops); };
+		if(tptr == NULL) free(c->ops);
 		c->ops = tptr;
-	};
-};
+	}
+}
 void CodeCol_add(PCodeCol c, BFOp op) {
 	BFOp* tptr;
 	c->ops[c->size] = op;
@@ -114,10 +114,10 @@ void CodeCol_add(PCodeCol c, BFOp op) {
 	if(c->size >= c->memSz) {
 		c->memSz <<= 1;
 		tptr = (BFOp*) realloc(c->ops, sizeof(BFOp) * c->memSz);
-		if(tptr == NULL) { free(c->ops); };
+		if(tptr == NULL) free(c->ops);
 		c->ops = tptr;
-	};
-};
+	}
+}
 void CodeCol_add_raw(PCodeCol c, OpCode o, int v) {
 	BFOp* tptr;
 	c->ops[c->size].ins = o;
@@ -126,17 +126,17 @@ void CodeCol_add_raw(PCodeCol c, OpCode o, int v) {
 	if(c->size >= c->memSz) {
 		c->memSz <<= 1;
 		tptr = (BFOp*) realloc(c->ops, sizeof(BFOp) * c->memSz);
-		if(tptr == NULL) { free(c->ops); };
+		if(tptr == NULL) free(c->ops);
 		c->ops = tptr;
-	};
-};
+	}
+}
 void CodeCol_free(PCodeCol c) {
 	if(c != NULL) {
-		if(c->ops != NULL) { free(c->ops); };
+		if(c->ops != NULL) free(c->ops);
 		free(c);
 		c = NULL;
-	};
-};
+	}
+}
 #ifdef LOWMEM
 typedef char CELL;
 #else
@@ -147,7 +147,7 @@ const int TapeSize = 1 << MEMORDER;
 const int TapeMask = (1 << MEMORDER) - 1;
 #endif
 PCodeCol loadSrc(const char* src) {
-	//Create variables
+	/* Create variables */
 	PCodeCol cc;
 	PIntStack ls;
 	FILE* inp;
@@ -156,106 +156,88 @@ PCodeCol loadSrc(const char* src) {
 	int cur = 0;
 	int count = 0;
 	int tmp = 0;
-	//Init variables
+	/* Init variables */
 	cc = CodeCol_new();
 	ls = IntStack_new();
-	//Open the file
+	/* Open the file */
 	inp = fopen(src, "r");
-	//Do checks
+	/* Do checks */
 	if((inp == NULL) || (cc == NULL) || (ls == NULL)) {
-		//Free any allocated memory
-		if(inp != NULL) { fclose(inp); };
-		if(cc != NULL) { CodeCol_free(cc); };
-		if(ls != NULL) { IntStack_free(ls); };
-		//Return a null pointer to indicate failiure
+		/* Free any allocated memory */
+		if(inp != NULL) fclose(inp);
+		if(cc != NULL) CodeCol_free(cc);
+		if(ls != NULL) IntStack_free(ls);
+		/* Return a null pointer to indicate failure */
 		return NULL; 
-	};
-	//Read code
+	}
+	/* Read code */
 	while(c != -1) {
-		//Get input
+		/* Get input */
 		c = fgetc(inp);
-		//Find instruction type
+		/* Find instruction type */
 		switch(c) {
-			case '+':
-				cur = 0;
-				break;
-			case '-':
-				cur = 0;
-				break;
-			case '<':
-				cur = 1;
-				break;
-			case '>':
-				cur = 1;
-				break;
-			case ',':
-				cur = 2;
-				break;
-			case '.':
-				cur = 3;
-				break;
-			case '[':
-				cur = 4;
-				break;
-			case ']':
-				cur = 5;
-				break;
-			default:
-				cur = -1;
-				break;
-		};
-		//Only parse instruction if it is recognizable or an EOF
+			case '+': cur = 0; break;
+			case '-': cur = 0; break;
+			case '<': cur = 1; break;
+			case '>': cur = 1; break;
+			case ',': cur = 2; break;
+			case '.': cur = 3; break;
+			case '[': cur = 4; break;
+			case ']': cur = 5; break;
+			default: cur = -1; break;
+		}
+		/* Only parse instruction if it is recognizable or an EOF */
 		if((cur != -1) || (c == -1)) {
-			//Flush runs to the code collection
+			/* Flush runs to the code collection */
 			if(cur != last) {
 				switch(last) {
 					case 0:
 						count &= 255;
-						if(count != 0) { CodeCol_add_raw(cc, ADJ, count); };
+						if(count != 0) CodeCol_add_raw(cc, ADJ, count);
 						count = 0;
 						break;
 					case 1:
-						if(count != 0) { CodeCol_add_raw(cc, MOV, count); };
+						if(count != 0) CodeCol_add_raw(cc, MOV, count);
 						count = 0;
 						break;
 					case 2:
-						if(count > 0) { CodeCol_add_raw(cc, INP, count); };
+						if(count > 0) CodeCol_add_raw(cc, INP, count);
 						count = 0;
 						break;
 					case 3:
-						if(count > 0) { CodeCol_add_raw(cc, OUT, count); };
+						if(count > 0) CodeCol_add_raw(cc, OUT, count);
 						count = 0;
 						break;
-				};
+				}
 				//Check memory
 				if(cc->ops == NULL) {
 					CodeCol_free(cc);
 					IntStack_free(ls);
 					fclose(inp);
 					return NULL;
-				};
-			};
+				}
+			}
 			//Parse the current instruction
 			switch(cur) {
 				case 0:
 					if(c == '+') {
-						count++;
+						++count;
 					} else if(c == '-') {
-						count--;
-					};
+						--count;
+					}
 					break;
 				case 1:
 					if(c == '>') {
-						count++;
+						++count;
 					} else if(c == '<') {
-						count--;
-					};
+						--count;
+					}
 					break;
 				case 2:
-					count++;
+					++count;
 					break;
 				case 3:
-					count++;
+					++count;
 					break;
 				case 4:
 					count = 0;
@@ -265,104 +247,104 @@ PCodeCol loadSrc(const char* src) {
 					count = 0;
 					CodeCol_add_raw(cc, FIN, 0);
 					break;
-			};
-			//Check memory
+			}
+			/* Check memory */
 			if(cc->ops == NULL) {
 					CodeCol_free(cc);
 					IntStack_free(ls);
 					fclose(inp);
 					return NULL;
-			};
-			//Update the last instruction
+			}
+			/* Update the last instruction */
 			last = cur;
-		};
-	};
-	//Close the source file
+		}
+	}
+	/* Close the source file */
 	fclose(inp);
-	//Convert [+] and [-] constructs into explicit zeros and merge ADJ and VAL operations
+	/* Convert [+] and [-] constructs into explicit zeros and merge ADJ and VAL operations */
 	cur = 0;
 	count = 0;
 	while(count < cc->size) {
-		//Check for a zeroing construct
+		/* Check for a zeroing construct */
 		if((count + 2) < cc->size) {
 			if((cc->ops[count].ins == BLK) && (cc->ops[count + 1].ins == ADJ) && (cc->ops[count + 2].ins == FIN)) {
-				//Overwrite previous null changes
+				/* Overwrite previous null changes */
 				if((count > 0) && (cur > 0)) {
-					if((cc->ops[count].ins == ADJ) || (cc->ops[count].ins == VAL)) { cur--; };
-				};
-				//Convert to an explicit VAL and merge any sequential ADJ operations
+					if((cc->ops[count].ins == ADJ) || (cc->ops[count].ins == VAL)) --cur;
+				}
+				/* Convert to an explicit VAL and merge any sequential ADJ operations */
 				if((count + 3) < cc->size) {
 					if(cc->ops[count + 3].ins == ADJ) {
-						//Create merged VAL
+						/* Create merged VAL */
 						cc->ops[cur].ins = VAL;
 						cc->ops[cur].val = cc->ops[count + 3].val;
 						count += 4;
 					} else {
-						//Create VAL instruction
+						/* Create VAL instruction */
 						cc->ops[cur].ins = VAL;
 						cc->ops[cur].val = 0;
 						count += 3;
-					};
+					}
 				} else {
-					//Create VAL instruction
+					/* Create VAL instruction */
 					cc->ops[cur].ins = VAL;
 					cc->ops[cur].val = 0;
 					count += 3;
-				};
+				}
 			} else if((cc->ops[cur].ins == ADJ) && (cc->ops[cur + 1].ins == ADJ)) {
-				//Merge matching ADJ operands
+				/* Merge matching ADJ operands */
 				cc->ops[cur].val = (cc->ops[cur].val + cc->ops[cur + 1].val) & 255;
 				count += 2;
 			} else {
-				//Copy instruction
+				/* Copy instruction */
 				cc->ops[cur].ins = cc->ops[count].ins;
 				cc->ops[cur].val = cc->ops[count].val;
-				count++;
-			};
+				++count;
+			}
 		} else if((count + 1) < cc->size) {
-			//Check for matching ADJ operands
+			/* Check for matching ADJ operands */
 			if((cc->ops[cur].ins == ADJ) && (cc->ops[cur + 1].ins == ADJ)) {
 				cc->ops[cur].val = (cc->ops[cur].val + cc->ops[cur + 1].val) & 255;
 				count += 2;
 			} else {
-				//Copy instruction
+				/* Copy instruction */
 				cc->ops[cur].ins = cc->ops[count].ins;
 				cc->ops[cur].val = cc->ops[count].val;
-				count++;
-			};
+				++count;
+			}
 		} else {
-			//Copy instruction
+			/* Copy instruction */
 			cc->ops[cur].ins = cc->ops[count].ins;
 			cc->ops[cur].val = cc->ops[count].val;
-			count++;
-		};
-		//Update loops
+			++count;
+		}
+		/* Update loops */
 		switch(cc->ops[cur].ins) {
 			case BLK:
 				IntStack_push(ls, cur);
 				break;
 			case FIN:
-				//If stack is empty, return null pointer
+				/* If stack is empty, return null pointer */
 				if(ls->stPtr == 0) {
 					CodeCol_free(cc);
 					IntStack_free(ls);
 					return NULL;
-				};
-				//Update the loop
+				}
+				/* Update the loop */
 				tmp = IntStack_pop(ls);
 				cc->ops[tmp].val = cur;
 				cc->ops[cur].val = tmp;
 				break;
-		};
-		//Check memory
+		}
+		/* Check memory */
 		if(ls->stack == NULL) {
 			IntStack_free(ls);
 			CodeCol_free(cc);
 			return NULL;
-		};
-		//Increment pointer
-		cur++;
-	};
+		}
+		/* Increment pointer */
+		++cur;
+	}
 	//Update code size
 	cc->size = cur;
 	//Check braces
@@ -370,7 +352,7 @@ PCodeCol loadSrc(const char* src) {
 		IntStack_free(ls);
 		CodeCol_free(cc);
 		return NULL;
-	};
+	}
 	//Cleanup
 	IntStack_free(ls);
 	CodeCol_trim(cc);
@@ -379,8 +361,8 @@ PCodeCol loadSrc(const char* src) {
 		return NULL;
 	} else {
 		return cc;
-	};
-};
+	}
+}
 int runCode(PCodeCol code) {
 	//Setup variables
 #ifdef INFINITE
@@ -397,11 +379,11 @@ int runCode(PCodeCol code) {
 	int ptr = TapeSize >> 1;
 	int idx = 0;
 	int tmp = 0;
-	//Check the tape
-	if(tape == NULL) { return 1; };
-	//Zero the tape
+	/* Check the tape */
+	if(tape == NULL) { return 1; }
+	/* Zero the tape */
 	memset(tape, 0, sizeof(CELL) * TapeSize);
-	//Execute the code
+	/* Execute the code */
 	while(idx < code->size) {
 		switch(code->ops[idx].ins) {
 			case ADJ:
@@ -429,13 +411,13 @@ int runCode(PCodeCol code) {
 						TapeSize += lexp;
 						lexp <<= 1;
 #endif
-					};
+					}
 					ttp = (CELL*) tape;
 					tape = (CELL*) malloc(sizeof(CELL) * TapeSize);
 					if(tape == NULL) {
 						free(ttp);
 						return 1;
-					};
+					}
 					memset(tape, 0, sizeof(CELL) * TapeSize);
 					memcpy((CELL*) tape + mod, ttp, sizeof(CELL) * old);
 					free(ttp);
@@ -452,46 +434,46 @@ int runCode(PCodeCol code) {
 						TapeSize += rexp;
 						rexp <<= 1;
 #endif
-					};
+					}
 					ttp = (CELL*) tape;
 					tape = (CELL*) malloc(sizeof(CELL) * TapeSize);
 					if(tape == NULL) {
 						free(ttp);
 						return 1;
-					};
+					}
 					memset(tape, 0, sizeof(CELL) * TapeSize);
 					memcpy((CELL*) tape + mod, ttp, sizeof(CELL) * old);
 					free(ttp);
 					ttp = NULL;
-				};
+				}
 #endif
 				break;
 			case INP:
 				tmp = code->ops[idx].val;
 				while(tmp > 0) {
 					tape[ptr] = (CELL) (getchar() & 255);
-					tmp--;
-				};
+					--tmp;
+				}
 				break;
 			case OUT:
 				tmp = code->ops[idx].val;
 				while(tmp > 0) {
 					putchar(tape[ptr]);
-					tmp--;
-				};
+					--tmp;
+				}
 				break;
 			case BLK:
-				if(!tape[ptr]) { idx = code->ops[idx].val; };
+				if(!tape[ptr]) idx = code->ops[idx].val;
 				break;
 			case FIN:
-				if(tape[ptr]) { idx = code->ops[idx].val; };
+				if(tape[ptr]) idx = code->ops[idx].val;
 				break;
-		};
-		idx++;
-	};
+		}
+		++idx;
+	}
 	free(tape);
 	return 0;
-};
+}
 int main(int argc, const char* argv[]) {
 	PCodeCol prog;
 	int idx = 0;
@@ -506,9 +488,9 @@ int main(int argc, const char* argv[]) {
 				err += runCode(prog);			
 			} else {
 				printf("%s: Error while loading\n", argv[idx]);
-				err++;
-			};
-		};
+				++err;
+			}
+		}
 		return err;
-	};
-};
+	}
+}
